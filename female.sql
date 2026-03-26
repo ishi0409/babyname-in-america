d7efa111bb25e9b55d858c4bb2464666dca46d8f
ishi0409 <isaku0409@gmail.com>
2026-03-26 20:55:19 +0900

    Create female.sql

---
+++ female.sql
@@ -0,0 +1,31 @@
+WITH f AS (
+ SELECT
+    state,
+    SUM(number) as total,
+    year,
+    name,
+    RANK() OVER(PARTITION BY state, year ORDER BY SUM(number) DESC) as rnk
+  FROM
+    `bigquery-public-data.usa_names.usa_1910_current`
+  WHERE
+    gender = "F"
+  GROUP BY
+    state, 
+    year,
+    name
+  ORDER BY
+    total DESC
+)
+
+SELECT
+  state,
+  name,
+  year
+FROM
+  f
+WHERE
+  rnk = 1
+ORDER BY
+  year,
+  state
+;
